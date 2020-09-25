db38dc458a6810052c6eae210c99d2d8b2d3754a
Demetris Konstantinides <demetris.konst@gmail.com>
2020-09-25 16:26:57 +0300

    Final Preps v3

--- src/routers/place.js
+++ src/routers/place.js
@@ -300,7 +300,7 @@ router.get('/place/:id/photos/main', async (req, res, next) => {
 
     let buffer;
     if (!place.photos || !place.photos.main) {
-      const photo = await Photo.find({default: true});
+      const photo = await Photo.findOne({default: true});
       buffer = photo.binary;
     }else{
       const photo = await Photo.findById(place.photos.main);
